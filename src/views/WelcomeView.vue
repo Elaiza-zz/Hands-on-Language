70c893110f98874418ab35796b24e9683d13bff0
Elaiza-zz <elaizacultura@gmail.com>
2024-12-12 02:22:29 +0800

    added branch

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -4,23 +4,20 @@ import imgHol from "@/assets/images/hol-logo1.jpg"
 
 <template>
   <div class="welcome-container">
-      <div class="container-fluid">
-        <div class="row justify-content-center">
-
-
-          <div class="col text-center">
-            <h1 class="card-titles">Welcome to Hands On Language</h1>
-          </div>
+    <div class="container-fluid d-flex flex-column justify-content-center align-items-center min-vh-85">
+      <div class="row justify-content-center">
+        <div class="col text-center">
+          <h1 class="card-titles">Welcome to Hands On Language</h1>
         </div>
       </div>
-
-    <div class="content">
-      <div class="button-container">
-        <button type="button" class="btn btn-primary">
-          <RouterLink class="text-light text-decoration-none" to="/login">
-            Getting Started
-          </RouterLink>
-        </button>
+      <div class="row mt-auto">
+        <div class="col text-center">
+          <button type="button" class="btn btn-primary">
+            <RouterLink class="text-light text-decoration-none" to="/login">
+              Getting Started
+            </RouterLink>
+          </button>
+        </div>
       </div>
     </div>
   </div>
