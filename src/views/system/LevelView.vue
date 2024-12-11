47ea4871008cf020f7adffe3e5b5fd625896d0b5
Elaiza-zz <elaizacultura@gmail.com>
2024-12-12 01:40:40 +0800

    added level view

---
+++ src/views/system/LevelView.vue
@@ -0,0 +1,15 @@
+<script>
+import imgFil from "@assets/images/filipino-flag.png"
+</script>
+
+<template>
+    <nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="imgFil/filipino-flag.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+      Filipino Sign Language
+    </a>
+  </div>
+</nav>
+
+</template>
