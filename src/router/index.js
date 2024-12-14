6bd490b2aa9a8393d885bacbf6b807deb958c87c
JumMy777 <jummy1806@gmail.com>
2024-12-15 01:49:40 +0800

    ++added path v2

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import WelcomeView from '../views/WelcomeView.vue'
 import LoginView from '../auth/LoginView.vue'
 import RegisterView from '../auth/RegisterView.vue'
 import HomeView from '../views/HomeView.vue'
+import LessonsView from '../views/system/LessonsView.vue'
+import LevelView from '../views/system/LevelView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -29,6 +31,18 @@ const router = createRouter({
       path: '/home',
       name: 'home',
       component: HomeView,
+      meta: { requiresAuth: true }, // Require authentication
+    },
+    {
+      path: '/lesson',
+      name: 'lesson',
+      component: LessonsView,
+      meta: { requiresAuth: true }, // Require authentication
+    },
+    {
+      path: '/level',
+      name: 'level',
+      component: LevelView,
       meta: { requiresAuth: true }, // Require authentication
     },
   ],
---
+++ src/views/system/LessonsView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>Lessons</h1>
+    <p>Here are the lessons</p>
+  </div>
+</template>
+```
